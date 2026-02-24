c831c00477619c55aa7c7481628e1969739a0f7d
CHOLYET\USER <ondagjuliet@gmail.com>
2026-02-24 22:33:01 +0800

    Router the Login

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Topvar } from './topvar/topvar';
+import { Sidenav } from './sidenav/sidenav';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Topvar, Sidenav],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
-
-}
+export class Home {}
