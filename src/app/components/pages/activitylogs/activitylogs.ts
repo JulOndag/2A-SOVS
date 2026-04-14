a7d301a2c1e8586017569e0b8fbd36d338dc7ab9
unabia.hazeljoy123@gmai.com <jillanekate119@gmail.com>
2026-04-15 00:24:39 +0800

    improved designs on the activty and result

--- src/app/components/pages/activitylogs/activitylogs.ts
+++ src/app/components/pages/activitylogs/activitylogs.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-activitylogs',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './activitylogs.html',
   styleUrl: './activitylogs.scss',
 })
 export class Activitylogs {
-
+  // Sample data - replace with real data from your service
+  activities = [
+    {
+      type: 'new',
+      message: 'New voter registered',
+      time: '10:20 AM'
+    },
+    {
+      type: 'vote',
+      message: '17 votes added to President',
+      time: '5 mins ago'
+    },
+    {
+      type: 'user',
+      message: 'User789 voted',
+      time: '10:15 AM'
+    },
+    {
+      type: 'admin',
+      message: 'Admin logged in',
+      time: '8:45 AM'
+    }
+  ];
 }
